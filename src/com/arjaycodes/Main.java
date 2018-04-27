aedd8d979b1bf619dbf6b62eca7b3266f01ed429
jsrj <rj.javadev@icloud.com>
2018-04-26 21:38:33 -0400

    added encoder instance and call to test method that writes the encoded string to a file in Sample_STLs

--- src/com/arjaycodes/Main.java
+++ src/com/arjaycodes/Main.java
@@ -1,4 +1,14 @@
 package com.arjaycodes;
 
+import com.arjaycodes.Base64.Encoder;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Encoder fileEncoder = new Encoder("/Users/jsrj/Documents/repos/stl-to-base64/Sample_STLs/SFD_Linked.stl");
+
+        //fileEncoder.getEncodedStringSegments();
+        fileEncoder.writeEncodedStringToFile();
+    }
 }
